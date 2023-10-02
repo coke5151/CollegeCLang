abaea5b323a94b7a0d46cfa1a5e68281928ec7f9
pytree <houjunqimail@gmail.com>
2023-10-02 21:39:08 +0800

    瘋狂解題

---
+++ OJ/10016.c
@@ -0,0 +1,26 @@
+// 賣油翁（我覺得這題題目敘述有問題）
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int n, x = 0;
+  scanf("%d", &n);
+  if (n <= 0) {
+    x = -100;
+  }
+  if (n > 0 && n % 3 == 0) {
+    x += 3;
+  }
+  if (n > 0 && n % 5 == 0) {
+    x += 5;
+  }
+
+  if (100 <= n && n <= 200) {
+    x += 50;
+  } else if (n > 0) {
+    x -= 50;
+  }
+  printf("%d", x);
+  return 0;
+}
---
+++ OJ/10017.c
@@ -0,0 +1,23 @@
+// 公鹿二連霸
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  if (n >= 50) {
+    printf("100");
+  } else if (n >= 45) {
+    printf("90");
+  } else if (n >= 40) {
+    printf("80");
+  } else if (n >= 35) {
+    printf("70");
+  } else if (n >= 30) {
+    printf("60");
+  } else {
+    printf("0");
+  }
+  return 0;
+}
