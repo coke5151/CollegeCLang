7b3c9383409aa3ed987a6738e028ae95430b3541
pytree <houjunqimail@gmail.com>
2023-10-20 09:36:59 +0800

    剩一題 m324 就可以破台了！

--- zerojudge/m324.c
+++ zerojudge/m324.c
@@ -1,7 +1,38 @@
 // m324. 小賴的期中作業(11的最大整數)
 
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+bool isfactor(int arr[], int n) {
+    int odd = 0, even = 0;
+    int minus = 0;
+    for (int i = 0; i < n; i += 2) {
+        odd += (arr[i] * pow(2, n - 1 - i));
+    }
+    for (int i = 1; i < n; i += 2) {
+        even += (arr[i] * pow(2, n - 1 - i));
+    }
+    minus = abs(odd - even);
+    return (minus % 11 == 0);
+}
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubble_sort(int arr[], int n) {
+    for (int j = n - 2; j >= 0; j--) {
+        for (int i = 0; i <= j; i++) {
+            if (arr[i] < arr[i + 1]) {
+                swap(&arr[i], &arr[i + 1]);
+            }
+        }
+    }
+}
 
 
 
@@ -12,6 +43,11 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &input[i]);
     }
+
+    bubble_sort(input, n);
+    // for (int i = 0; i < n; i++) {
+    //     printf("%d", input[i]);
+    // }
 
     return 0;
 }
